c944684c9a4a23922d5edc5c7c7fec6480684320
Louis Jennings <60139957+louisjennings@users.noreply.github.com>
2023-04-02 09:54:10 +1200

    Add additional history data to console output Hope to use it in future for drawing map of specific cleaning session

--- src/purei9_unofficial/__main__.py
+++ src/purei9_unofficial/__main__.py
@@ -198,7 +198,7 @@ if args.command == "cloud":
             OUTPUT = []
             
             i = 0
-            for sess in map(lambda x: {"endtime": x.endtime.isoformat(), "duration": x.duration, "cleandearea": x.cleandearea, "imageurl": x.imageurl, "endstatus": x.endstatus}, rc.getCleaningSessions()):
+            for sess in map(lambda x: {"sessionid": x.sessionid, "endtime": x.endtime.isoformat(), "duration": x.duration, "cleandearea": x.cleandearea, "imageurl": x.imageurl, "endstatus": x.endstatus, "mapid": x.mapid, "mapsn": x.mapsn}, rc.getCleaningSessions()):
                 
                 if args.output == "table":
                     from .imageascii import draw2shade
